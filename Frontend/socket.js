e246dd2f920e89432b56c06d1054eabc9c6c3359
kumarsatwik <38569860+Kumarsatwik@users.noreply.github.com>
2024-01-26 02:37:50 +0530

    Update socket.js

--- Frontend/socket.js
+++ Frontend/socket.js
@@ -6,7 +6,7 @@ export const initSocket = async () => {
     timeout: 10000,
     transports: ["websocket"],
   };
-    console.log(`Connecting to ${import.meta.env.VITE_REACT_APP_BACKEND_URL}`);
+    // console.log(`Connecting to ${import.meta.env.VITE_REACT_APP_BACKEND_URL}`);
 
   return io(import.meta.env.VITE_REACT_APP_BACKEND_URL, options);
 };
